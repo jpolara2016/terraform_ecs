27e8bd26e4d9bac366e89cf1e925f4251ed69d8c
jpolara2016 <jpolara2016@my.fit.edu>
2021-03-28 23:13:46 -0400

    Modified user-data

--- modules/alb/cloud-config.sh
+++ modules/alb/cloud-config.sh
@@ -2,7 +2,7 @@
 
 # Install awslogs and the jq JSON parser
 yum update -y
-yum install -y awslogs jq aws-cli
+yum install -y awslogs jq aws-cli tar screen
 
 systemctl stop ecs
 
@@ -18,16 +18,20 @@ EOF
 systemctl start ecs
 
 # Install Filebeat to forward and centralize logs and files 
-
+sudo -su ec2-user
 curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.12.0-linux-x86_64.tar.gz
 tar xzvf filebeat-7.12.0-linux-x86_64.tar.gz
-chown root -R filebeat-7.12.0-linux-x86_64
-filebeat-7.12.0-linux-x86_64; rm -rf filebeat.yaml
-curl -O https://jpolata-filebeat.s3.amazonaws.com/filebeat.yaml
-cd modules.d/
-./filebeat modules enable system
-curl -O https://jpolata-filebeat.s3.amazonaws.com/system.yaml
+sudo chown root -R filebeat-7.12.0-linux-x86_64
+cd filebeat-7.12.0-linux-x86_64; rm -rf filebeat.yml
+sudo curl https://jpolata-filebeat.s3.amazonaws.com/filebeat.yaml -o filebeat.yml
+sudo chown root filebeat.yml
+sudo chmod go-w filebeat.yml
+sudo ./filebeat modules enable system
+sudo curl https://jpolata-filebeat.s3.amazonaws.com/system.yaml -o modules.d/system.yml
+sudo ./filebeat setup -e
+
 
 screen -S filebeat
-./filebeat setup -e
+sudo ./filebeat -e
+
 
